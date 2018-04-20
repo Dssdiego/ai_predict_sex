0e1534674c68df08fa04bc45759e5e527ca551f9
Diego Santos Seabra <dss-diego@hotmail.com>
2018-04-20 14:43:50 -0300

    Comments in the Python / README Files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-ai_predict_sex
+# ai_predict_sex
+
+## How to Use
+
+Change the values for the weight and the height in the "Change Here" part of the code
+
--- main.py
+++ main.py
@@ -12,57 +12,40 @@ from sklearn.linear_model import LinearRegression
 # Read the CSV File
 df = pd.read_csv('data.csv')
 
-# ax = sb.barplot(x="sex", y="height", data=df)
-# ax = sb.barplot(x="sex", y="weight", data=df)
-
-# df.plot(kind='pie', subplots=True, autopct='%1.1f%%',
-#  startangle=90, shadow=False, labels=df['sex'], legend = False, fontsize=14)
-
-# df.groupby(df['sex']).sum().plot(kind='pie', subplots=True, autopct='%1.1f%%')
-# py.axis('equal')
-
 # Replaces Male -> 0
 df['sex'] = df['sex'].replace('Male', 0)
 
 # Replaces Female -> 1
 df['sex'] = df['sex'].replace('Female', 1)
 
-X = df[['weight','height']]
+# Sets the X and Y Dataframe Values
+X = df[['weight', 'height']]
 Y = df[['sex']]
 
-X_train, X_test , y_train , y_test = train_test_split(X,Y)
+# Trains the A.I.
+X_train, X_test, y_train, y_test = train_test_split(X, Y)
 
-# print(x_train)
-# print(x_test)
-# print(y_train)
-# print(y_test)
-
-# print(X_train.shape)
-# print(y_train.shape)
-
+# Fits the data in the Linear Regression Model
 model = LinearRegression()
 model.fit(X_train, y_train)
 
-predictions=model.predict(X_test)
+# Tests the Model
+predictions = model.predict(X_test)
 
-# print(X_test)
-
-# print(predictions)
-
-# g = sb.lmplot(x="weight", y="height", hue="sex",
-#                truncate=True, size=5, data=df)
-
-# g = sb.lmplot(x="weight", y="height", hue="weight",
-#               truncate=True, size=5, data=df)
-
-# sb.distplot(y_test-predictions, axlabel="{x, y}")
-# py.xlabel("Colors")
-# py.ylabel("Values")
+# Plots the Test and Predictions Chart
+sb.distplot(y_test - predictions, axlabel="{x, y}")
+py.xlabel("Colors")
+py.ylabel("Values")
 
 py.show()
 
-myvals = np.array([94,182]).reshape(1,-1)
+# Predicts the Sex given the Input (Change Here)
+# 94 -> 94 Kg (Weight)
+# 182 -> 182 cm (Height)
+
+myvals = np.array([94, 182]).reshape(1, -1)
 print(model.predict(myvals))
 
-# Prints the Data
-# print(df)
+# The more close to 0, the more is a Male
+# The more close to 1, the more is a Female
+
